295aff7d17600f291a2a97a49b85a6f85b6a5424
KARENTHETHIRD <ojukwukaren@yahoo.com>
2020-08-16 09:47:05 -0400

    first commit

---
+++ src/pages/dashboard/components/am4chartMap/mock.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    latitude: 53.450140,
+    longitude: -6.264255,
+    size: 6,
+    tooltip: '79.97.106.65',
+    fill: "#F00"
+  },
+{
+    latitude: 53.350140,
+    longitude: -6.266155,
+    size: 4,
+    tooltip: '192.141.28.11',
+    fill: "#F00"
+  },
+{
+    latitude: 53.581788,
+    longitude: -6.266155,
+    size: 6,
+    tooltip: '185.40.67.12',
+    fill: "#F00"
+  },
+
+{
+    latitude: 53.557333,
+    longitude: -6.266155,
+    size: 8,
+    tooltip: '185.40.67.59',
+    fill: "#F00"
+  },
+    {
+    latitude: 53.575643,
+    longitude: -6.262345,
+    size: 8,
+    tooltip: '52.31.51.10',
+    fill: "#F00"
+  },
+
+]
